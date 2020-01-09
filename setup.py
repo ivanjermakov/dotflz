9812fd2693dccadf9d06cba8fd5a79af00b5a3bc
ivanjermakov <vania6600@gmail.com>
2020-01-09 15:35:02 +0300

    publishing preparation

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+# Use README for the PyPI page
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='dotflz',
     version='0.1',
@@ -9,5 +13,8 @@ setup(
     author='Ivan Ermakov',
     author_email='ivanjermakov1@gmail.com',
     description='Utility to keep copies of dotfiles in one place',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3',
     install_requires=['wheel', 'pyyaml', 'glob3', 'more-itertools', 'datetime', 'click']
 )
