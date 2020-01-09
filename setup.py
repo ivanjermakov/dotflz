c792e7f7713ed8014be92fee27da79f00ec618c3
ivanjermakov <vania6600@gmail.com>
2020-01-09 14:59:25 +0300

    dependency management

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(
     author='Ivan Ermakov',
     author_email='ivanjermakov1@gmail.com',
     description='Utility to keep copies of dotfiles in one place',
+    install_requires=['wheel', 'pyyaml', 'glob3', 'more-itertools', 'datetime', 'click']
 )
