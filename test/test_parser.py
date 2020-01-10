4c027be1688ffca3d84ea2acd36d21d775b18489
ivanjermakov <vania6600@gmail.com>
2020-01-10 22:40:03 +0300

    improve parser tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -34,8 +34,8 @@ class TestParser(unittest.TestCase):
         self.assertEqual(1, len(config.items))
         item = config.items[0]
         self.assertEqual('a', item.name)
-        self.assertEqual('a', item.frm)
-        self.assertEqual('a', item.to)
+        self.assertEqual('a/', item.frm)
+        self.assertEqual('a/', item.to)
         self.assertEqual(0, len(item.files))
 
     def test_parse_config_4(self):
