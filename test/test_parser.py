e010a6bdb755777bf83c7cd268fff53c6817184c
ivanjermakov <vania6600@gmail.com>
2020-01-10 23:21:14 +0300

    test refactoring

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,8 +1,11 @@
 import unittest
 
 from dotflz.parser import *
-from test import utils
 from test.commons import *
+
+
+def get_config_path(config_number):
+    return f'../config/{config_number}.yml'
 
 
 class TestParser(unittest.TestCase):
@@ -19,18 +22,18 @@ class TestParser(unittest.TestCase):
         Parse configuration file 1
         """
         with self.assertRaises(AttributeError):
-            parse_config(utils.get_config_path(1))
+            parse_config(get_config_path(1))
 
     def test_parse_empty_valid_config(self):
         """
         Parse configuration file 2
         """
-        config = parse_config(utils.get_config_path(2))
+        config = parse_config(get_config_path(2))
         self.assertEqual('dotfiles/', config.name)
         self.assertEqual(0, len(config.items))
 
     def test_parse_config_3(self):
-        config = parse_config(utils.get_config_path(3))
+        config = parse_config(get_config_path(3))
         self.assertEqual(1, len(config.items))
         item = config.items[0]
         self.assertEqual('a', item.name)
@@ -39,25 +42,25 @@ class TestParser(unittest.TestCase):
         self.assertEqual(0, len(item.files))
 
     def test_parse_config_4(self):
-        config = parse_config(utils.get_config_path(4))
+        config = parse_config(get_config_path(4))
         item = config.items[0]
         self.assertEqual('a/', item.to)
 
     def test_parse_config_5(self):
-        config = parse_config(utils.get_config_path(5))
+        config = parse_config(get_config_path(5))
         item = config.items[0]
         self.assertEqual(2, len(item.files))
         self.assertEqual('c.html', item.files[0])
         self.assertEqual('c.txt', item.files[1])
 
     def test_parse_config_6(self):
-        config = parse_config(utils.get_config_path(6))
+        config = parse_config(get_config_path(6))
         self.assertEqual(4, len(config.items))
         self.assertEqual(2, len(config.items[3].files))
         self.assertEqual('ca/c.html', config.items[2].files[0])
         self.assertEqual('b.xml', config.items[3].files[1])
 
     def test_parse_config_7(self):
-        config = parse_config(utils.get_config_path(7))
+        config = parse_config(get_config_path(7))
         self.assertEqual(3, len(config.items[2].files))
         self.assertEqual(2, len(config.items[3].files))
