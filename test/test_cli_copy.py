600f2eb0fce2bd43145c190f3927d3272010a784
ivanjermakov <ivanjermakov1@gmail.com>
2020-02-29 14:53:20 +0300

    improve tests

--- test/test_cli_copy.py
+++ test/test_cli_copy.py
@@ -4,12 +4,30 @@ from dotflz.cli import *
 from dotflz.parser import *
 from test.commons import *
 
+DOTFILES_DIR_PATH = 'dotfiles/'
+ON = '../test_dir'
+
 
 def get_config_path(config_number):
     return f'../config/{config_number}.yml'
 
 
-class TestCliVerify(unittest.TestCase):
+def count_files(path):
+    return sum([len(files) for r, d, files in os.walk(path)])
+
+
+def print_tree():
+    click.echo('tree:')
+    for root, dirs, files in os.walk(DOTFILES_DIR_PATH):
+        level = root.count(os.sep) if os.path.basename(root) else 0
+        indent = '└───' * level
+        click.echo('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = '    ' * (level) + '└───'
+        for f in files:
+            click.echo('{}{}'.format(subindent, f))
+
+
+class TestCliCopy(unittest.TestCase):
     PWD = os.getcwd()
 
     def setUp(self) -> None:
@@ -17,27 +35,73 @@ class TestCliVerify(unittest.TestCase):
         os.chdir(DYN_TEST_DIR_PATH)
 
     def tearDown(self) -> None:
-        os.chdir(TestCliVerify.PWD)
+        os.chdir(TestCliCopy.PWD)
         delete_file(DYN_TEST_DIR_PATH)
 
     def test_copy1(self):
         with self.assertRaises(Exception):
-            copy(get_config_path(1))
+            copy(get_config_path(1), on=ON)
+
+    def test_copy1clean(self):
+        with self.assertRaises(Exception):
+            copy(get_config_path(1), True, on=ON)
 
     def test_copy2(self):
-        copy(get_config_path(2))
+        copy(get_config_path(2), on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy2clean(self):
+        copy(get_config_path(2), True, on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
 
     def test_copy3(self):
-        copy(get_config_path(3))
+        copy(get_config_path(3), on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy3clean(self):
+        copy(get_config_path(3), True, on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
 
     def test_copy4(self):
-        copy(get_config_path(4))
+        copy(get_config_path(4), on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy4clean(self):
+        copy(get_config_path(4), True, on=ON)
+        print_tree()
+        self.assertEqual(0, count_files(DOTFILES_DIR_PATH))
 
     def test_copy5(self):
-        copy(get_config_path(5))
+        copy(get_config_path(5), on=ON)
+        print_tree()
+        self.assertEqual(2, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy5clean(self):
+        copy(get_config_path(5), True, on=ON)
+        print_tree()
+        self.assertEqual(2, count_files(DOTFILES_DIR_PATH))
 
     def test_copy6(self):
-        copy(get_config_path(6))
+        copy(get_config_path(6), on=ON)
+        print_tree()
+        self.assertEqual(6, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy6clean(self):
+        copy(get_config_path(6), True, on=ON)
+        print_tree()
+        self.assertEqual(6, count_files(DOTFILES_DIR_PATH))
 
     def test_copy7(self):
-        copy(get_config_path(7))
+        copy(get_config_path(7), on=ON)
+        print_tree()
+        self.assertEqual(7, count_files(DOTFILES_DIR_PATH))
+
+    def test_copy7clean(self):
+        copy(get_config_path(7), True, on=ON)
+        print_tree()
+        self.assertEqual(7, count_files(DOTFILES_DIR_PATH))
--- test/test_parser.py
+++ test/test_parser.py
@@ -10,6 +10,7 @@ def get_config_path(config_number):
 
 class TestParser(unittest.TestCase):
     PWD = os.getcwd()
+    ON = '.'
 
     def setUp(self) -> None:
         os.chdir(TEST_DIR_PATH)
@@ -22,45 +23,45 @@ class TestParser(unittest.TestCase):
         Parse configuration file 1
         """
         with self.assertRaises(AttributeError):
-            parse_config(get_config_path(1))
+            parse_config(get_config_path(1), TestParser.ON)
 
     def test_parse_empty_valid_config(self):
         """
         Parse configuration file 2
         """
-        config = parse_config(get_config_path(2))
+        config = parse_config(get_config_path(2), TestParser.ON)
         self.assertEqual('dotfiles/', config.name)
         self.assertEqual(0, len(config.items))
 
     def test_parse_config_3(self):
-        config = parse_config(get_config_path(3))
+        config = parse_config(get_config_path(3), TestParser.ON)
         self.assertEqual(1, len(config.items))
         item = config.items[0]
         self.assertEqual('a', item.name)
-        self.assertEqual('a/', item.frm)
+        self.assertEqual('./a/', item.frm)
         self.assertEqual('a/', item.to)
         self.assertEqual(0, len(item.files))
 
     def test_parse_config_4(self):
-        config = parse_config(get_config_path(4))
+        config = parse_config(get_config_path(4), TestParser.ON)
         item = config.items[0]
         self.assertEqual('a/', item.to)
 
     def test_parse_config_5(self):
-        config = parse_config(get_config_path(5))
+        config = parse_config(get_config_path(5), TestParser.ON)
         item = config.items[0]
         self.assertEqual(2, len(item.files))
         self.assertEqual('c.html', item.files[0])
         self.assertEqual('c.txt', item.files[1])
 
     def test_parse_config_6(self):
-        config = parse_config(get_config_path(6))
+        config = parse_config(get_config_path(6), TestParser.ON)
         self.assertEqual(4, len(config.items))
         self.assertEqual(2, len(config.items[3].files))
         self.assertEqual('ca/c.html', config.items[2].files[0])
         self.assertEqual('b.xml', config.items[3].files[1])
 
     def test_parse_config_7(self):
-        config = parse_config(get_config_path(7))
+        config = parse_config(get_config_path(7), TestParser.ON)
         self.assertEqual(3, len(config.items[2].files))
         self.assertEqual(2, len(config.items[3].files))
