d3762f0a9b7454ef8214a8128bc56f6135c7316e
ivanjermakov <ivanjermakov1@gmail.com>
2020-03-05 10:41:38 +0300

    v0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='dotflz',
-    version='0.2',
+    version='0.3',
     packages=['dotflz'],
     url='https://github.com/ivanjermakov/dotflz',
     license='MIT',
