d39ccb13ce3913b20790e946054a5fcd7ced5114
ivanjermakov <ivanjermakov1@gmail.com>
2020-03-05 10:33:05 +0300

    fix option name

--- dotflz/dotflz.py
+++ dotflz/dotflz.py
@@ -14,7 +14,7 @@ def dotflz():
 @dotflz.command(short_help='Copy files by specified configuration file')
 @click.argument('config_path')
 @click.option('-c', '--clean', is_flag=True, help='Delete files missing from config file.')
-@click.option('-o', '-on', help='Context path for relative configuration "from" paths')
+@click.option('-o', '--on', help='Context path for relative configuration "from" paths')
 def copy(config_path, clean, on):
     """
     Copy files by according to CONFIG_PATH config file.
@@ -24,7 +24,7 @@ def copy(config_path, clean, on):
 
 @dotflz.command(short_help='Replace original files with ones from configured directory.')
 @click.argument('config_path')
-@click.option('-o', '-on', help='Context path for relative configuration "from" paths')
+@click.option('-o', '--on', help='Context path for relative configuration "from" paths')
 def paste(config_path, on):
     """
     Replace original files with ones from configured directory according to CONFIG_PATH config file.
@@ -34,7 +34,7 @@ def paste(config_path, on):
 
 @dotflz.command(short_help='Verify configuration file.')
 @click.argument('config_path')
-@click.option('-o', '-on', help='Context path for relative configuration "from" paths')
+@click.option('-o', '---on', help='Context path for relative configuration "from" paths')
 def verify(config_path, on):
     """
     Verify configuration file from CONFIG_PATH.
@@ -48,7 +48,7 @@ def verify(config_path, on):
 @click.argument('config_path')
 @click.option('-d', '--dir', help='Backup directory name. Timestamp if not specified.')
 @click.option('-c', '--clean', is_flag=True, help='Delete backup directory before copying if such exist.')
-@click.option('-o', '-on', help='Context path for relative configuration "from" paths')
+@click.option('-o', '--on', help='Context path for relative configuration "from" paths')
 def backup(config_path, dir, clean, on):
     """
     Backup original files into backup directory according to CONFIG_PATH config file.
@@ -59,7 +59,7 @@ def backup(config_path, dir, clean, on):
 @dotflz.command(short_help='Restore original files with specified backup directory.')
 @click.argument('config_path')
 @click.argument('backup_dir_name')
-@click.option('-o', '-on', help='Context path for relative configuration "from" paths')
+@click.option('-o', '--on', help='Context path for relative configuration "from" paths')
 def restore(config_path, backup_dir_name, on):
     """
     Restore original files from BACKUP_DIR_NAME according to CONFIG_PATH config file.
